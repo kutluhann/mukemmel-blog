db67811e88e8323f4729259be4975dbbe2a72fb1
kutluhann <kutluhan22@gmail.com>
2020-01-30 22:49:08 +0300

    Arama işlemi rafa kaldırıldı :)

--- components/blog/BlogPage.js
+++ components/blog/BlogPage.js
@@ -1,20 +1,11 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import PostList from '../../components/blog/PostList'
 import Pagination from '../../components/blog/Pagination'
-import SearchField from '../../components/blog/SearchField'
 import Categories from '../../components/blog/Categories'
 
 const BlogPage = ({ posts, categories, isFirst, hasMore }) => {
   const { query } = useRouter()
-  const [blogPosts, setBlogPosts] = useState([])
-  const [searchVal, setSearchVal] = useState('')
-  const getSearchVal = (searchVal) => setSearchVal(searchVal)
-  useEffect(() => {
-    const result = searchVal.length ? posts.filter(post => post.title.includes(searchVal)) : posts
-    setBlogPosts(result)
-  }, [searchVal, posts])
   return (
     <main className="pt-12 px-6">
       <section>
@@ -26,12 +17,11 @@ const BlogPage = ({ posts, categories, isFirst, hasMore }) => {
         <div className="container flex">
           <div className="flex touch:flex-col-reverse w-full desktop:w-10/12 widescreen:w-9/12 mx-auto">
             <div className="w-2/3 touch:w-full h-12 desktop:mx-3 touch:my-2 h-auto relative">
-              <PostList posts={blogPosts} />
+              <PostList posts={posts} />
               <Pagination page={query.page} categorySlug={query.categorySlug} isFirst={isFirst} hasMore={hasMore} />
             </div>
             <aside className="w-1/3 touch:w-full desktop:mx-3 touch:mb-2 h-auto">
               <div className="sticky top-6 w-full flex flex-col justify-satart items-center touch:flex-col-reverse">
-                <SearchField passSearchValue={getSearchVal} categorySlug={query.categorySlug} categories={categories} />
                 <Categories categories={categories} categorySlug={query.categorySlug} />
               </div>
             </aside>
